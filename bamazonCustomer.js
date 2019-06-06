4baffbc807d2b1eaf33378270315db43fd406e88
Darren Pham <phamdarren1@gmail.com>
2019-06-06 03:50:04 -0700

    created inquirer function that requests purchase order from user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,14 +20,52 @@ var bamazonCustomer = {
         query = 'SELECT id, product_name, price FROM products WHERE stock_quantity > 0'
         connection.query(query, function (err, results, fields) {
             if (err) throw err;
+            console.log('ID  Product Name         Price')
             for (var i=0; i < results.length; i++) {
                 console.log(results[i].id, results[i].product_name, results[i].price)
             }
-            
-        
+            bamazonCustomer.purchase()
         });
     },
+    purchase : function() {
+        inquirer
+        .prompt([{
+            message: 'Enter id of item you would like to purchase:',
+            name: 'itemID'
+        },
+        {
+            message: 'How much would you like to purchase?',
+            name: 'amount',
+            validate: function(input) {
+                var done = this.async();
+                setTimeout(function() {
+                    if (isNaN(input)) {
+                        done('You need enter a number');
+                        return;
+                    }
+                    done(null, true);
+                }, 1000);
+            }
+        }])
+        .then(answers => {
+            query = 'SELECT stock_quantity, price FROM products WHERE id = ' + answers.itemID
+            connection.query(query, function (err, results, fields) {
+                if (err) throw err;
+                // console.log('Sending purchase order')
+                if (results[0].stock_quantity < answers.amount) {
+                    console.log('Purchase Failed')
+                    console.log('Sorry! We only have ' +results[0].stock_quantity+ ' left in stock')
+                } else {
+                    bamazonCustomer.completeOrder(answers.itemID, answers.amount)
+                    
+                }
+            });
+        });
+    },
+    completeOrder : function(productID, amount) {
 
+        console.log('Purchase complete! \nThank you for your business (^-^)/')
+    }
 }
 
 bamazonCustomer.listStock()
